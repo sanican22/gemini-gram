8dfa9763db61607eb333baeede8bb1f22bec1a0e
sani can <sanimustafa33@gmail.com>
2025-07-11 01:15:20 +0300

    geer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.tsx",
+    "./App.tsx",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}"
   ],
